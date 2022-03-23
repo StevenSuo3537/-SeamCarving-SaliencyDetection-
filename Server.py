997871a3a76cac0a6e7624e6a1fb56dcfcbfc8ab
StevenSuo3537 <58136439+StevenSuo3537@users.noreply.github.com>
2022-03-23 13:14:28 +0800

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,46 @@
+# This Python file uses the following encoding: utf-8
+from PyQt5 import QtWidgets
+from window import Ui_Window
+from PyQt5.QtWidgets import QFileDialog
+import pandas as pd
+import cair_al
+import cv2
+
+class Server(QtWidgets.QWidget, Ui_Window):
+    def __init__(self):
+        super(Server, self).__init__()
+        self.setupUi(self)
+        self.pushButton_3.clicked.connect(self.read_file)
+        self.pushButton.clicked.connect(self.process)
+
+    def read_file(self): #选取文件
+        filename, filetype =QFileDialog.getOpenFileName(self, "选取文件", "C:/", "Jpg Files(*.jpg)")
+        print(filename, filetype)
+        self.lineEdit.setText(filename)
+
+    def process(self):
+        if self.comboBox.currentText() == "横向":
+            which_axis = 'c'
+        else:
+            which_axis == 'r'
+        scale = float(self.lineEdit_2.text())
+        in_filename = self.lineEdit.text()
+        out_filename = self.lineEdit_3.text()
+
+        img = cv2.imread(in_filename)
+
+        if which_axis == 'r':
+            out = cair_al.crop_r(img, scale)
+        elif which_axis == 'c':
+            out = cair_al.crop_c(img, scale)
+
+        cv2.imwrite(out_filename,out)
+        cv2.imshow("output", out)
+        cv2.waitKey(0)
+
+if __name__=="__main__":
+     import sys
+     app=QtWidgets.QApplication(sys.argv)
+     ui = Server()
+     ui.show()
+     sys.exit(app.exec_())
---
+++ cair_al.py
@@ -0,0 +1,79 @@
+# This Python file uses the following encoding: utf-8
+
+# if__name__ == "__main__":
+#     pass
+import sys
+import cv2
+
+from tqdm import trange
+import numpy as np
+
+def calc_energy(img):
+    saliency = cv2.saliency.StaticSaliencySpectralResidual_create()
+    (success, saliencyMap) = saliency.computeSaliency(img)
+    saliencyMap = (saliencyMap * 255).astype("uint8")
+    #cv2.imshow("Image", img)
+    #cv2.imshow("Output", saliencyMap)
+    #cv2.waitKey(0)
+
+    return saliencyMap
+
+#按列裁剪
+def crop_c(img, scale_c):
+    r, c, _ = img.shape
+    new_c = int(scale_c * c)
+    if scale_c <= 1:
+        for i in trange(c - new_c):
+            img = carve_column(img)
+    else:
+
+        img = cv2.resize(img, (c+2*(new_c-c) , r), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite("img.jpg",img)
+        for i in trange(new_c-c):
+            img = carve_column(img)
+
+    return img
+#按行裁剪
+def crop_r(img, scale_r):
+    img = np.rot90(img, 1, (0, 1))
+    img = crop_c(img, scale_r)
+    img = np.rot90(img, 3, (0, 1))
+    return img
+
+def carve_column(img):
+    r, c, _ = img.shape
+
+    M, backtrack = minimum_seam(img)
+    mask = np.ones((r, c), dtype=np.bool)
+
+    j = np.argmin(M[-1])
+    for i in reversed(range(r)):
+        mask[i, j] = False
+        j = backtrack[i, j]
+
+    mask = np.stack([mask] * 3, axis=2)
+    img = img[mask].reshape((r, c - 1, 3))
+    return img
+
+def minimum_seam(img):
+    r, c, _ = img.shape
+    energy_map = calc_energy(img)
+    M = energy_map
+    backtrack = np.zeros_like(M, dtype=np.int)
+
+    for i in range(1, r):
+        for j in range(0, c):
+            # 处理图像的左侧边缘，确保不会索引-1
+            if j == 0:
+                idx = np.argmin(M[i-1, j:j + 2])
+                backtrack[i, j] = idx + j
+                min_energy = M[i-1, idx + j]
+            else:
+                idx = np.argmin(M[i - 1, j - 1:j + 2])
+                backtrack[i, j] = idx + j - 1
+                min_energy = M[i - 1, idx + j - 1]
+
+            M[i, j] += min_energy
+
+    return M, backtrack
+
